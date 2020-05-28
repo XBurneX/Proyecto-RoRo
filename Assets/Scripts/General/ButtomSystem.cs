66a2137bce5a03f54921ec8b3f07588fcafdef81
Alejandro <killer.jano1470@gmail.com>
2020-05-28 05:12:21 -0500

    escenas y manejo de escenas + transcicion

---
+++ Assets/Scripts/General/ButtomSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtomSystem : MonoBehaviour
+{
+    public GameObject someButton;
+    GameObject currentButton;
+    EventSystem eventSystem;
+    void Start()
+    {
+        eventSystem = GameObject.Find("EventSystem").GetComponent<EventSystem>();
+        //GameObject buttonObj = GameObject.Find("Play");
+        //buttonObj.GetComponent<Button>().onClick.AddListener(() => { currentButton = buttonObj; });
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            eventSystem.SetSelectedGameObject(someButton);
+        }
+    }
+}
