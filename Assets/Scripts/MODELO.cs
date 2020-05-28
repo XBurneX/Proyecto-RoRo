28afd7fd7662ad1c123ff49cf0d76521d39a170f
Sebastian LLerena Cruz <65472493+NoRespaw@users.noreply.github.com>
2020-05-28 13:12:46 -0500

    Se añade scrip modelo al proyecto

---
+++ Assets/Scripts/MODELO.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MODELO : MonoBehaviour {
+   
+	public float live;
+	public string name;
+	public int peso ;
+	public float velocidad;
+	public int traccion ;
+	public float cooldown ;
+
+    // tengo sueño
+    void Start() {
+
+    }
+    // causa
+    void Update() {
+
+
+    }
+}
