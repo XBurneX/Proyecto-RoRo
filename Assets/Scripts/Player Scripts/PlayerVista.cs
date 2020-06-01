86fd57cb7d4f8411f69bb7c6c0fb9cdda48c51bf
Jafet <andresjeyminez@gmail.com>
2020-06-01 00:29:46 -0500

    Vistaplayer

---
+++ Assets/Scripts/Player Scripts/PlayerVista.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerVista : MonoBehaviour
+{
+    public Image BarraDeVida;
+
+    public Image HabilidadCooldown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<PlayerController>().actualLive = Mathf.Clamp(GetComponent<PlayerController>().actualLive, 0, GetComponent<MODELO>().live * GetComponent<PlayerController>().liveModifier);
+        BarraDeVida.fillAmount = GetComponent<PlayerController>().actualLive / (GetComponent<MODELO>().live * GetComponent<PlayerController>().liveModifier);
+
+        HabilidadCooldown.fillAmount = GetComponent<PlayerController>().timer / GetComponent<MODELO>().cooldown;
+    }
+}
