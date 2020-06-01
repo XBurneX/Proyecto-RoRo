05ec772ddb104ba243dcf80cd3bc1a6a2aebef2d
Jafet <andresjeyminez@gmail.com>
2020-06-01 01:15:35 -0500

    JabaliModelo

---
+++ Assets/Scripts/Jabali/JabaliModelo.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JabaliModelo : MonoBehaviour
+{
+    public bool alerta;
+    public float velocidad;
+    Rigidbody rb;
+    public float daño;
+    public float cooldown;
+    public float distancia;
+    public float timer;
+
+    //por si te faltan variables usa estas XD
+    public bool variable1;
+    public bool variable2;
+    public int variable3;
+    public int variable4;
+    public int variable5;
+    public int variable6;
+    public float variable7;
+    public float variable8;
+    public string variable9;
+    public string variable10;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
