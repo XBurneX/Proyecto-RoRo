ab3f67e84c43f6a9021363f9a3b8f6360d4acfd0
sebastianllerenacruz <sebastianllerenacruz@gmail.com>
2020-06-10 01:45:04 -0500

    Controlador de enemigos

---
+++ Assets/Scripts/Jabali/JabaliControlador.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class JabaliControlador : MonoBehaviour { 
+
+
+public GameObject Target;
+public NavMeshAgent agent;
+public float distance;
+    //public Health healScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(Target.transform.position, transform.position)< distance) {
+            agent.SetDestination(Target.transform.position);
+            agent.speed = 10;
+        } else {
+            agent.speed = 0;
+        //}
+        //if (Vector3.Distance(Target.transform.position,transform.position) <= 2) {
+            //healScript.health = healScript.health - 5; 
+        }
+    }
+}
