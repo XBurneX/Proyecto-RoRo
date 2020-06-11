7fa4159006ce8adf7de8c74365bd0dccb31f6017
Alejandro <killer.jano1470@gmail.com>
2020-06-11 07:41:24 -0500

    Menu de pausa y randomizador de textos

---
+++ Assets/Scripts/General/Random_Frase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Random_Frase : MonoBehaviour
+{
+    public Text texttext;
+    public GameObject targettext;
+
+    public string tx;
+
+    public string[] T_Frase;
+
+    // Use this for initialization
+    void Start()
+    {
+        texttext = targettext.GetComponent<Text>();
+
+        tx = T_Frase[Random.Range(0,T_Frase.Length)];
+        texttext.text = tx;
+    }
+}
