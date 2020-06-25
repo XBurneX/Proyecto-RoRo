da1d053dffe7851d6822ddfc839d08c2abdb2dee
Alejandro <killer.jano1470@gmail.com>
2020-06-25 09:49:50 -0500

    Selector de personaje y habilidad/item

---
+++ Assets/Scripts/Data_Select/NameSel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class NameSel : MonoBehaviour
+{
+    public Text n1;
+    public Text n2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GameObject.Find("UserStatsInfo"))
+        {
+            n1.text = GameObject.Find("UserStatsInfo").GetComponent<UsersInfo>().pName1;
+            n2.text = GameObject.Find("UserStatsInfo").GetComponent<UsersInfo>().pName2;
+        }
+        else
+        {
+            Debug.Log("NonDataObject");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Data_Select/Selector.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Selector : MonoBehaviour
+{
+    public bool pj_itm;
+    public bool t1_f2;
+
+    public int selectedNum;
+
+    public Text texto;
+    public Text descripcion;
+    public Sprite[] miniatura;
+    public string[] mini_name;
+    public string[] description;
+ 
+    int[] selctNums;
+
+    public string textData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectedNum = 0;
+        GameObject.Find("UserStatsInfo").GetComponent<UsersInfo>().pChar1 = "Tugfa";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Image>().sprite = miniatura[selectedNum];
+        texto.text = mini_name[selectedNum];
+        descripcion.text = description[selectedNum];
+
+        UpData();
+    }
+
+    void UpData()
+    {
+        if (pj_itm == true)
+        {
+            if (t1_f2==true)
+            {
+                GameObject.Find("UserStatsInfo").GetComponent<UsersInfo>().pChar1 = textData;
+            }
+            else
+            {
+                GameObject.Find("UserStatsInfo").GetComponent<UsersInfo>().pChar2 = textData;
+            }
+        }
+        else
+        {
+            if (t1_f2 == true)
+            {
+                GameObject.Find("UserStatsInfo").GetComponent<UsersInfo>().pHab1 = textData;
+            }
+            else
+            {
+                GameObject.Find("UserStatsInfo").GetComponent<UsersInfo>().pHab2 = textData;
+            }
+        }
+        
+    }
+
+    public void Next(int dir)
+    {
+        if (pj_itm == true)
+        {
+            selectedNum += dir;
+
+            if(selectedNum>=miniatura.Length)
+            {
+                selectedNum = 0;
+            }
+            else if(selectedNum<0)
+            {
+                selectedNum = miniatura.Length-1;
+            }
+        }
+
+        else
+        {
+
+        }
+    }
+}
