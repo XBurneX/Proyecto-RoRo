28a814022205eba64b58b86bf95b7d415711c06d
Alejandro <killer.jano1470@gmail.com>
2020-07-09 10:33:56 -0500

    Progrmacion de generador de nivel a base de modulos, implementacion de algunos assets

---
+++ Assets/Scripts/General/MetrsModls.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetrsModls : MonoBehaviour
+{
+    public float x_mod_mt;
+    public float z_mod_mt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/General/ModularGenerator.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModularGenerator : MonoBehaviour
+{
+    public Vector3 debugPosition;
+
+    public GameObject startMod;
+    public GameObject endMod;
+
+
+    public GameObject[] verticalMods;
+
+    public int distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+        debugPosition = transform.position;
+
+     
+
+        Generate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+  
+    }
+
+    void Generate()
+    {
+        Debug.Log("Generate");
+       
+
+        for (int i = 0; i <= distance; i++)
+        {
+            GameObject actualMod;
+
+            Debug.Log("Before select");
+
+            if (i == 0)
+            {
+                actualMod = startMod;
+            }
+
+            else if (i == distance)
+            {
+                actualMod = endMod;
+            }
+
+            else
+            {
+                actualMod = verticalMods[Random.Range(0, verticalMods.Length)];
+            }
+            Debug.Log("After Select");
+
+            debugPosition = new Vector3(
+                debugPosition.x + (actualMod.GetComponent<MetrsModls>().x_mod_mt * 10),
+                debugPosition.y,
+                debugPosition.z + (actualMod.GetComponent<MetrsModls>().z_mod_mt * 10));
+
+            GameObject modular = Instantiate(actualMod);
+
+            modular.transform.position = debugPosition;
+            Debug.Log(i);
+
+            debugPosition = new Vector3(
+                debugPosition.x + (actualMod.GetComponent<MetrsModls>().x_mod_mt * 10),
+                debugPosition.y,
+                debugPosition.z + (actualMod.GetComponent<MetrsModls>().z_mod_mt * 10));
+
+        }
+    }
+}
