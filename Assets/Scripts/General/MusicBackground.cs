50e1fb051f0229466002513d6415bc91486cae3c
Alejandro <killer.jano1470@gmail.com>
2020-07-16 09:00:36 -0500

    Armado de nivel 1, actualizacion de modulos, script de reproductor de musica

---
+++ Assets/Scripts/General/MusicBackground.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicBackground : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static MusicBackground backgroundMusicScript;
+
+    private void Awake()
+    {
+        if (!backgroundMusicScript)
+        {
+            backgroundMusicScript = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
